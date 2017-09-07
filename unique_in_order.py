3f43a026b934f92595a57764fd3f2a1ed4ed7695
Eric Allain <ejallain@gmail.com>
2017-09-07 13:05:26 -0400

    trivial changes to unique_in_order to test out git

--- unique_in_order.py
+++ unique_in_order.py
@@ -8,12 +8,12 @@ Created on Fri Apr  7 09:49:38 2017
 
 def unique_in_order(iterable):
     return([iterable[n] for n,_ in enumerate(iterable) if n == 0 or iterable[n-1] != iterable[n]])
-    
-    
-    
-    
-    
-    
-print(unique_in_order('AAAABBBCCDAABBBA'))
+
+
+
+
+
+
+print(unique_in_order('AAAABBBCCDAABBBACC'))
 print(unique_in_order(''))
 print(unique_in_order('A'))
